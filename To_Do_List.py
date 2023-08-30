33da1aaf565e8969da0e0c94bca8ccd63a22048c
MUHAMMAD-HADI-RAZA <raza4503871@cloud.neduet.edu.pk>
2023-08-30 23:32:14 +0500

    initial commit

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,36 +1,42 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 
 class TO_DO_APP:
     def __init__(self, root):
         self.root = root
         self.root.title("T0-D0 LIST APP")
 
+        self.root.configure(bg="#e0f2f1")
+
         self.Tasks = []
 
-        self.Task_Label = tk.Label(root, text="Task:")
+        self.Task_Label = ttk.Label(root, text="Task:")
         self.Task_Label.pack(pady=(10, 0))
 
-        self.Task_Entry = tk.Entry(root)
+        self.Task_Entry = ttk.Entry(root)
         self.Task_Entry.pack(pady=(0, 10))
 
-        self.Due_Date_Label = tk.Label(root, text="Due Date (DD-MM-YYYY):")
+        self.Due_Date_Label = ttk.Label(root, text="Due Date (DD-MM-YYYY):")
         self.Due_Date_Label.pack()
 
-        self.Due_Date_Entry = tk.Entry(root)
+        self.Due_Date_Entry = ttk.Entry(root)
         self.Due_Date_Entry.pack()
 
-        self.Add_Button = tk.Button(root, text="Add Task", command=self.Add_Task)
+        self.Add_Button = ttk.Button(root, text="Add Task", command=self.Add_Task, style="Custom.TButton")
         self.Add_Button.pack()
 
-        self.Task_ListBox = tk.Listbox(root)
+        self.Task_ListBox = tk.Listbox(root, selectbackground="royalblue1")
         self.Task_ListBox.pack(pady=(10, 0))
 
-        self.Complete_Button = tk.Button(root, text="Mark as Completed", command=self.Complete_Task)
+        self.Complete_Button = ttk.Button(root, text="Mark as Completed", command=self.Complete_Task, style="Custom.TButton")
         self.Complete_Button.pack()
 
-        self.Delete_Button = tk.Button(root, text="Delete Task", command=self.Delete_Task)
+        self.Delete_Button = ttk.Button(root, text="Delete Task", command=self.Delete_Task, style="Custom.TButton")
         self.Delete_Button.pack()
+
+        self.custom_button_style = ttk.Style()
+        self.custom_button_style.configure("Custom.TButton", font=("Xenara", 10), background="#009688")
 
     def Add_Task(self):
         Task = self.Task_Entry.get()
